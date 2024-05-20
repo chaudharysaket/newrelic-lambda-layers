b603768fe742fbb011799688539efa52bd6eccda
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-05-21 00:52:13 +0530

    permission denied

--- libBuild.sh
+++ libBuild.sh
@@ -4,55 +4,55 @@ set -Eeuo pipefail
 
 # Regions that support arm64 architecture
 REGIONS_ARM=(
-  af-south-1
-	ap-northeast-1
-	ap-northeast-2
-	ap-northeast-3
-	ap-south-1
-	ap-southeast-1
-	ap-southeast-2
-	ap-southeast-3
-	ca-central-1
-	eu-central-1
-	eu-north-1
-	eu-south-1
-	eu-west-1
-	eu-west-2
-	eu-west-3
-	me-south-1
-	sa-east-1
+  # af-south-1
+	# ap-northeast-1
+	# ap-northeast-2
+	# ap-northeast-3
+	# ap-south-1
+	# ap-southeast-1
+	# ap-southeast-2
+	# ap-southeast-3
+	# ca-central-1
+	# eu-central-1
+	# eu-north-1
+	# eu-south-1
+	# eu-west-1
+	# eu-west-2
+	# eu-west-3
+	# me-south-1
+	# sa-east-1
 	us-east-1
-	us-east-2
-	us-west-1
+	# us-east-2
+	# us-west-1
 	us-west-2
 )
 
 REGIONS_X86=(
-  af-south-1
-  ap-northeast-1
-  ap-northeast-2
-  ap-northeast-3
-  ap-south-1
-  ap-south-2
-  ap-southeast-1
-  ap-southeast-2
-  ap-southeast-3
-  ap-southeast-4
-  ca-central-1
-  eu-central-1
-  eu-central-2
-  eu-north-1
-  eu-south-1
-  eu-south-2
-  eu-west-1
-  eu-west-2
-  eu-west-3
-  me-central-1
-  me-south-1
-  sa-east-1
+  # af-south-1
+  # ap-northeast-1
+  # ap-northeast-2
+  # ap-northeast-3
+  # ap-south-1
+  # ap-south-2
+  # ap-southeast-1
+  # ap-southeast-2
+  # ap-southeast-3
+  # ap-southeast-4
+  # ca-central-1
+  # eu-central-1
+  # eu-central-2
+  # eu-north-1
+  # eu-south-1
+  # eu-south-2
+  # eu-west-1
+  # eu-west-2
+  # eu-west-3
+  # me-central-1
+  # me-south-1
+  # sa-east-1
   us-east-1
-  us-east-2
-  us-west-1
+  # us-east-2
+  # us-west-1
   us-west-2
 )
 
@@ -217,16 +217,16 @@ function publish_layer {
 
     hash=$( hash_file $layer_archive | awk '{ print $1 }' )
 
-    bucket_name="nr-layers-${region}"
+    bucket_name="nr-test-saket-layers-${region}"
     s3_key="$( s3_prefix $runtime_name )/${hash}.${arch}.zip"
 
     compat_list=( $runtime_name )
     if [[ $runtime_name == "provided" ]]
-    then compat_list=("provided" "provided.al2" "provided.al2023" "dotnetcore3.1" "dotnet6")
+    then compat_list=("provided" "provided.al2" "provided.al2023" "dotnetcore3.1" "dotnet6" "python3.6" )
     fi
 
     if [[ $runtime_name == "dotnet" ]]
-    then compat_list=("dotnet6" "dotnet7" "dotnet8")
+    then compat_list=("dotnet6" "dotnet8")
     fi
 
     echo "Uploading ${layer_archive} to s3://${bucket_name}/${s3_key}"
