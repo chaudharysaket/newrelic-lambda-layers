208690845375264c844cbf837c34e3dca287cb9e
Saket Chaudhary <saketchaudharynit@gmail.com>
2025-03-17 22:12:43 +0530

    mkdir folder

--- nodejs/publish-layers.sh
+++ nodejs/publish-layers.sh
@@ -31,7 +31,9 @@ function build_wrapper {
   echo "Installed newrelic package"
   NEWRELIC_AGENT_VERSION=$(npm list newrelic --prefix $BUILD_DIR | grep newrelic@ | awk -F '@' '{print $2}')
   echo "Installed New Relic version: $NEWRELIC_AGENT_VERSION"
-  echo "NEWRELIC_AGENT_VERSION=$NEWRELIC_AGENT_VERSION" > /nr-env-var
+  mkdir -p nr-env-var
+  pwd
+  echo "NEWRELIC_AGENT_VERSION=$NEWRELIC_AGENT_VERSION" > nr-env-var
   mkdir -p $BUILD_DIR/node_modules/newrelic-lambda-wrapper
   cp index.js $BUILD_DIR/node_modules/newrelic-lambda-wrapper
   mkdir -p $BUILD_DIR/node_modules/newrelic-esm-lambda-wrapper
