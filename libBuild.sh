e6d224f9baf3bbbbb9fb5f26c6fd111f7ec597e0
Saket Chaudhary <saketchaudharynit@gmail.com>
2025-03-21 10:52:11 +0530

    publish layers

--- libBuild.sh
+++ libBuild.sh
@@ -4,56 +4,56 @@ set -Eeuo pipefail
 
 # Regions that support arm64 architecture
 REGIONS_ARM=(
-	af-south-1
-	ap-northeast-1
-	ap-northeast-2
-	ap-northeast-3
-	ap-south-1
-	ap-southeast-1
-	ap-southeast-2
-	ap-southeast-3
-	ca-central-1
-	eu-central-1
-	eu-north-1
-	eu-south-1
-	eu-west-1
-	eu-west-2
-	eu-west-3
-	me-south-1
-	sa-east-1
+	# af-south-1
+	# ap-northeast-1
+	# ap-northeast-2
+	# ap-northeast-3
+	# ap-south-1
+	# ap-southeast-1
+	# ap-southeast-2
+	# ap-southeast-3
+	# ca-central-1
+	# eu-central-1
+	# eu-north-1
+	# eu-south-1
+	# eu-west-1
+	# eu-west-2
+	# eu-west-3
+	# me-south-1
+	# sa-east-1
 	us-east-1
-	us-east-2
-	us-west-1
-	us-west-2
+	# us-east-2
+	# us-west-1
+	# us-west-2
 )
 
 REGIONS_X86=(
-  af-south-1
-  ap-northeast-1
-  ap-northeast-2
-  ap-northeast-3
-  ap-south-1
-  ap-south-2
-  ap-southeast-1
-  ap-southeast-2
-  ap-southeast-3
-  ap-southeast-4
-  ca-central-1
-  eu-central-1
-  eu-central-2
-  eu-north-1
-  eu-south-1
-  eu-south-2
-  eu-west-1
-  eu-west-2
-  eu-west-3
-  me-central-1
-  me-south-1
-  sa-east-1
+  # af-south-1
+  # ap-northeast-1
+  # ap-northeast-2
+  # ap-northeast-3
+  # ap-south-1
+  # ap-south-2
+  # ap-southeast-1
+  # ap-southeast-2
+  # ap-southeast-3
+  # ap-southeast-4
+  # ca-central-1
+  # eu-central-1
+  # eu-central-2
+  # eu-north-1
+  # eu-south-1
+  # eu-south-2
+  # eu-west-1
+  # eu-west-2
+  # eu-west-3
+  # me-central-1
+  # me-south-1
+  # sa-east-1
   us-east-1
-  us-east-2
-  us-west-1
-  us-west-2
+  # us-east-2
+  # us-west-1
+  # us-west-2
 )
 
 EXTENSION_DIST_DIR=extensions
@@ -71,8 +71,7 @@ function list_all_regions {
 
 function fetch_extension {
     arch=$1
-
-    url="https://github.com/newrelic/newrelic-lambda-extension/releases/download/v${EXTENSION_VERSION}/newrelic-lambda-extension.${arch}.zip"
+    url="https://github.com/chaudharysaket/newrelic-lambda-extension/releases/download/v4.0.0_extension/newrelic-lambda-extension.${arch}.zip"
     rm -rf $EXTENSION_DIST_DIR $EXTENSION_DIST_ZIP
     curl -L $url -o $EXTENSION_DIST_ZIP
 }
@@ -223,7 +222,7 @@ function publish_layer {
 
     hash=$( hash_file $layer_archive | awk '{ print $1 }' )
 
-    bucket_name="nr-layers-${region}"
+    bucket_name="nr-test-saket-layers-${region}"
     s3_key="$( s3_prefix $runtime_name )/${hash}.${arch}.zip"
 
     compat_list=( $runtime_name )
