1ecfb2e584f85f8259ca06dc30d97bf0a060d6fe
Saket Chaudhary <saketchaudharynit@gmail.com>
2025-03-21 11:22:43 +0530

    update ecr image path

--- libBuild.sh
+++ libBuild.sh
@@ -305,7 +305,7 @@ function publish_docker_ecr {
 
     # public ecr repository name 
     # maintainer can use this("q6k3q1g1") repo name for testing 
-    repository="x6n7b2o2"
+    repository="q6k3q1g1"
 
     # copy dockerfile
     cp ../Dockerfile.ecrImage .
