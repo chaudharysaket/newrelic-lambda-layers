0cdc864beb949671cee274691154df97af586481
Saket Chaudhary <saketchaudharynit@gmail.com>
2025-03-25 19:44:12 +0530

    apm layer release

--- libBuild.sh
+++ libBuild.sh
@@ -71,8 +71,9 @@ function list_all_regions {
 
 function fetch_extension {
     arch=$1
-
-    url="https://github.com/newrelic/newrelic-lambda-extension/releases/download/v${EXTENSION_VERSION}/newrelic-lambda-extension.${arch}.zip"
+    # This is APM+Lambda Fusion Extension for testing
+    url="https://github.com/chaudharysaket/newrelic-lambda-extension/releases/download/v2.4.0/newrelic-lambda-extension.${arch}.zip"
+    # url="https://github.com/newrelic/newrelic-lambda-extension/releases/download/v${EXTENSION_VERSION}/newrelic-lambda-extension.${arch}.zip"
     rm -rf $EXTENSION_DIST_DIR $EXTENSION_DIST_ZIP
     curl -L $url -o $EXTENSION_DIST_ZIP
 }
