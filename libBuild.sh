80c1be199ff2bf841501c8a1cdef9d1ff97cf316
Saket Chaudhary <saketchaudharynit@gmail.com>
2025-09-08 16:53:41 +0530

    test layers for nodejs16

--- libBuild.sh
+++ libBuild.sh
@@ -3,31 +3,31 @@
 set -Eeuo pipefail
 
 REGIONS=(
-  sa-east-1
-  me-central-1
-  me-south-1
-  eu-central-2
-  eu-north-1
-  eu-south-2
-  eu-west-3
-  eu-south-1
-  eu-west-2
-  eu-west-1
-  eu-central-1
-  ca-central-1
-  ap-northeast-1
-  ap-southeast-2
-  ap-southeast-1
-  ap-northeast-2
-  ap-northeast-3
-  ap-south-1
-  ap-south-2
-  ap-southeast-4
-  ap-southeast-3
-  af-south-1
+  # sa-east-1
+  # me-central-1
+  # me-south-1
+  # eu-central-2
+  # eu-north-1
+  # eu-south-2
+  # eu-west-3
+  # eu-south-1
+  # eu-west-2
+  # eu-west-1
+  # eu-central-1
+  # ca-central-1
+  # ap-northeast-1
+  # ap-southeast-2
+  # ap-southeast-1
+  # ap-northeast-2
+  # ap-northeast-3
+  # ap-south-1
+  # ap-south-2
+  # ap-southeast-4
+  # ap-southeast-3
+  # af-south-1
   us-east-1
-	us-east-2
-	us-west-1
+	# us-east-2
+	# us-west-1
 	us-west-2
 )
 
@@ -257,7 +257,7 @@ function publish_layer {
 
     hash=$( hash_file $layer_archive | awk '{ print $1 }' )
 
-    bucket_name="nr-layers-${region}"
+    bucket_name="nr-test-saket-layers-${region}"
     s3_key="$( s3_prefix $runtime_name )/${hash}.${arch}.zip"
 
     compat_list=( $runtime_name )
